b066a8c151c9984133db3c05f623b6505d3653e5
Shaojung <shaojung@gmail.com>
2016-12-09 10:03:14 +0800

    make receiver

--- app/src/main/java/com/example/user/yvd120901/DevicesActivity.java
+++ app/src/main/java/com/example/user/yvd120901/DevicesActivity.java
@@ -2,29 +2,66 @@ package com.example.user.yvd120901;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.ParcelUuid;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.UUID;
 
 public class DevicesActivity extends AppCompatActivity {
     final int REQUEST_ENABLE_BT = 321;
     MyReceiver mReceiver;
+    BluetoothAdapter mBtAdapter;
+    UUID mUUID;
+    BluetoothDevice myDevice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_devices);
         mReceiver = new MyReceiver();
-        BluetoothAdapter mBtAdapter = BluetoothAdapter.getDefaultAdapter( );
+        mBtAdapter = BluetoothAdapter.getDefaultAdapter( );
+
         if( !mBtAdapter.isEnabled() ) {
             Intent enableIntent = new Intent( BluetoothAdapter.ACTION_REQUEST_ENABLE );
             startActivityForResult( enableIntent, REQUEST_ENABLE_BT ); }
 
+    }
+
+    public void click1(View v)
+    {
         mBtAdapter.startDiscovery();
+    }
+
+    public void click2(View v)
+    {
+        BluetoothSocket clienSocket= null;
+        try {
+            clienSocket = myDevice.createRfcommSocketToServiceRecord(mUUID);
+            clienSocket.connect();
+
+            InputStream is = clienSocket.getInputStream();
+            OutputStream os = clienSocket.getOutputStream();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
         // 搜尋到藍牙裝置時，呼叫我們的函式
         IntentFilter filter = new IntentFilter( BluetoothDevice.ACTION_FOUND );
         this.registerReceiver( mReceiver, filter );
@@ -32,8 +69,14 @@ public class DevicesActivity extends AppCompatActivity {
         // 搜尋的過程結束後，也呼叫我們的函式
         filter = new IntentFilter( BluetoothAdapter.ACTION_DISCOVERY_FINISHED );
         this.registerReceiver( mReceiver, filter );
-
     }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        this.unregisterReceiver(mReceiver);
+    }
+
     class MyReceiver extends BroadcastReceiver
     {
         @Override
@@ -44,17 +87,23 @@ public class DevicesActivity extends AppCompatActivity {
             String action = intent.getAction();
             //找到設備
             if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                Log.d("BT", "Action Found");
                 BluetoothDevice device = intent
                         .getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
                 if (device.getBondState() != BluetoothDevice.BOND_BONDED) {
                     Log.d("BT", "find device:" + device.getName()
                             + device.getAddress());
+
                     Toast.makeText(DevicesActivity.this, "device:" + device.getName()
                             + device.getAddress(), Toast.LENGTH_SHORT).show();
+                    ParcelUuid[] uuids = device.getUuids();
+                    mUUID = uuids[0].getUuid();
+                    myDevice = device;
                 }
             }
         }
     }
+
 }
 
 
